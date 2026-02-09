8743a282dc015375e1d0cb1ca2dd6fce0db1345e
RaphaelP3 <112487127+RaphaelP3@users.noreply.github.com>
2026-02-09 14:20:37 +0100

    premier commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const formRecette = document.getElementById('form-recette')
+const nomRecette = document.getElementById('nom')
+const recette = document.getElementById('recette')
+const btnForm = document.getElementById('btn-form')
+
