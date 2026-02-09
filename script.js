bd70c82ce277f8e88320b4f67c67bf46df961abe
RaphaelP3 <112487127+RaphaelP3@users.noreply.github.com>
2026-02-09 15:48:59 +0100

    affichage recette

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+//Const formulaire
 const formRecette = document.getElementById('form-recette')
 const nomRecette = document.getElementById('nom')
 const recette = document.getElementById('recette')
+const plat = document.getElementById('plat')
 const btnForm = document.getElementById('btn-form')
-const afficherRecette = document.getElementById('result')
+const afficherTout = document.getElementById('result')
 let recettesFinies = JSON.parse(localStorage.getItem('users')) || [];
+//Const pour l'affichage de la recette
+const afficherNom = document.getElementById('afficher-nom')
+const afficherRecette = document.getElementById('afficher-recette')
+const afficherPlat = document.getElementById('afficher-plat')
+const favorisBouton = document.getElementById('favoris-btn')
+const supprimerBouton = document.getElementById('supprimer-btn')
+
 
 
 formRecette.addEventListener('submit', function (e) {
@@ -11,15 +20,37 @@ formRecette.addEventListener('submit', function (e) {
 
     const recetteFinie = {
         nom : nomRecette.value, 
-        recette : recette.value
+        recette : recette.value,
+        plat : plat.value
     }
 
     recettesFinies.push(recetteFinie)
     sauvegardeRecette(); 
+    Recette();
+    formRecette.reset();
 })
+
+Recette(); 
 
 function sauvegardeRecette() {
     localStorage.setItem('recettesFinies', JSON.stringify(recettesFinies))
 }
 
 
+function Recette() {
+    afficherNom.innerHTML=''
+    afficherRecette.innerHTML=''
+    afficherPlat.innerHTML=''
+    favorisBouton.innerHTML=''
+    supprimerBouton.innerHTML=''
+
+
+    recettesFinies.forEach((recetteFinie) => {
+        afficherNom.innerHTML += `<h2>${recetteFinie.nom}</h2>`
+        afficherRecette.innerHTML += `<p>${recetteFinie.recette}</p>`
+        afficherPlat.innerHTML += `<p>${recetteFinie.plat}</p>`
+        favorisBouton.innerHTML += `<button>Favoris</button>`
+        supprimerBouton.innerHTML += `<button>Supprimer</button>`
+
+    })
+}
