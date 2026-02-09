2d2d8c2de678a1fdb918eda2db464c6c0834eb70
RaphaelP3 <112487127+RaphaelP3@users.noreply.github.com>
2026-02-09 14:39:06 +0100

    formulaire + localStorage

--- script.js
+++ script.js
@@ -2,4 +2,24 @@ const formRecette = document.getElementById('form-recette')
 const nomRecette = document.getElementById('nom')
 const recette = document.getElementById('recette')
 const btnForm = document.getElementById('btn-form')
+const afficherRecette = document.getElementById('result')
+let recettesFinies = JSON.parse(localStorage.getItem('users')) || [];
+
+
+formRecette.addEventListener('submit', function (e) {
+    e.preventDefault(); 
+
+    const recetteFinie = {
+        nom : nomRecette.value, 
+        recette : recette.value
+    }
+
+    recettesFinies.push(recetteFinie)
+    sauvegardeRecette(); 
+})
+
+function sauvegardeRecette() {
+    localStorage.setItem('recettesFinies', JSON.stringify(recettesFinies))
+}
+
 
